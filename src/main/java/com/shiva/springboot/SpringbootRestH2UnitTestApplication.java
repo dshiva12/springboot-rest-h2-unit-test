ba879d3c11089c31907ff11f787bfa2deec9b3ed
Shivanand Deore <deore_shiva@rediffmail.com>
2022-10-10 20:19:49 +1100

    initial commit

---
+++ src/main/java/com/shiva/springboot/SpringbootRestH2UnitTestApplication.java
@@ -0,0 +1,13 @@
+package com.shiva.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootRestH2UnitTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootRestH2UnitTestApplication.class, args);
+	}
+
+}
